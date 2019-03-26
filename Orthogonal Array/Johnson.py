48b5396237d3c4609e1861b06403241c921d2dfc
Brian Kodalen <bgkodalen@wpi.edu>
2019-03-26 13:53:38 -0400

    all work cpu

--- Orthogonal Array/Johnson.py
+++ Orthogonal Array/Johnson.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 from itertools import combinations
 
-def Johnson_adj(n,k):
+def Johnson_adj(n,k=2):
     combs = list(combinations([i for i in range(n)],k))
     bincombs = np.array([[1 if i in tup else 0 for i in range(n)] for tup in combs])
     A = np.dot(bincombs,np.transpose(bincombs))
